250bf0e5ca437525ea6c1a6c02fc7e8b5af568b9
PiyaphatC <>
2026-04-23 15:16:21 +0700

    day X

---
+++ count_dup.py
@@ -0,0 +1,8 @@
+nums = [2,2,3,4,5,6,21,2,1]
+count = 0
+emp_lst = [] 
+for num in nums:
+    if count < 2 or emp_lst[count - 2] != num:
+        emp_lst.append(num)
+        count = count + 1
+nums[:] = emp_lst
